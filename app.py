80514637cb9bf7621774ef5b66630792b6a043d8
Aziz Kandemir <azizkandemiir@gmail.com>
2019-05-18 03:58:22 +0300

    Added Packet Generator and Node.

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from flask import request, jsonify, render_template
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
